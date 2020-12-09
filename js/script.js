715ba6277b24683275775f1aa43a9186a6aab794
Luca Cavretti <cavretiluca@live.it>
2020-12-09 17:54:07 +0100

    Finished first two sections

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 const myApp = new Vue({
     el: '#root',
-    data: {},
+    data: {
+        topUl: [{
+                name: 'home',
+                link: '#'
+            },
+            {
+                name: 'who we are',
+                link: '#'
+            },
+            {
+                name: 'what we do',
+                link: '#'
+            },
+            {
+                name: 'where we work',
+                link: '#'
+            },
+            {
+                name: 'careers',
+                link: '#'
+            },
+            {
+                name: 'news',
+                link: '#'
+            }
+        ]
+    },
     methods: {},
 
 })
